1a726952db8e95ef1101ce307f7883b3c5c3a632
Hassaan Mirza <84412764+BlankRelax@users.noreply.github.com>
2022-12-04 15:45:20 +0000

    Finished code with tabulation

---
+++ main.py
@@ -0,0 +1,29 @@
+from tabulate import tabulate
+j1= 7
+j2=-3
+n = abs(j1+j2)
+j3=0
+j4=0
+j5=0
+j6=0
+x=-10
+count = 0
+data = []
+for j3 in range(x,-x):
+    for j4 in range(x,-x):
+        for j5 in range(x, -x):
+            for j6 in range(x, -x):
+                if abs(j3)+abs(j4)+abs(j5)+abs(j6)==n and (abs(j5)+abs(j6) == 0 or (abs(j5)+abs(j6))%2==0) and j1+j2+j3+j4+j5+j6==0:
+                    temp_dat = []
+                    temp_dat.append(j1)
+                    temp_dat.append(j2)
+                    temp_dat.append(j3)
+                    temp_dat.append(j4)
+                    temp_dat.append(j5)
+                    temp_dat.append(j6)
+                    data.append(temp_dat)
+                    #print("j1= "+str(j1)+" j2= "+str(j2)+" j3= "+str(j3)+" j4= "+str(j4)+" j5= "+str(j5)+" j6= "+str(j6))
+                    #count+=1
+
+col_names = ["j1", "j2", "j3","j4", "j5", "j6"]
+print(tabulate(data, headers=col_names, tablefmt="fancy_grid", showindex="always"))
